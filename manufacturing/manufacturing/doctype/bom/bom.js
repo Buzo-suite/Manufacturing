603f50f187fc30f8d69c22912f5fabe5bc7c4303
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2023-07-16 14:05:11 +0530

    BOM changes

--- manufacturing/manufacturing/doctype/bom/bom.js
+++ manufacturing/manufacturing/doctype/bom/bom.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2023, Buzosuite and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("BOM", {
-// 	refresh(frm) {
+frappe.ui.form.on("BOM", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
