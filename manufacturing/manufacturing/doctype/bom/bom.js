7c5d9366313221feccff9c433c1cad61de972ccb
ajith1999 <ajithkumarmariyappan@gmail.com>
2023-08-21 22:22:16 +0530

    Filters for BOM

--- manufacturing/manufacturing/doctype/bom/bom.js
+++ manufacturing/manufacturing/doctype/bom/bom.js
@@ -10,20 +10,32 @@ frappe.ui.form.on("BOM", {
 				}
 			}
 		})
-        frm.set_query('item', (doc)=>{
-			return{
-				filters: {
-					"item_group": "Finished-product"
-				}
-			}
-		})
 	},
+	item: function(frm) {
+		console.log("Hello");
+		frm.fields_dict['items'].grid.get_field('item').get_query = function(doc, cdt, cdn) {
+            var d = frm.doc.item;
+			var existing_items = [];
+
+            $.each(doc.items || [], function(i, item) {
+               if (item.item !== d) {
+                  existing_items.push(item.item);
+            }
+        });
+            return {
+                filters: [
+                    ['Item', 'name', '!=', d],
+					['Item', 'name', 'not in', existing_items]
+                ]
+            };
+        };
+    }
 });
 
 frappe.ui.form.on("BOM Item","qty",(frm,cdt,cdn)=>{
 	let child_table = locals[cdt][cdn];
 	let calculate_amount = flt(child_table.qty * child_table.rate);
-	console.log('hello',child_table.qty * child_table.rate);
+	//console.log('hello',child_table.qty * child_table.rate);
 	child_table.amount = calculate_amount;
 	frm.refresh_field('amount');
 });
